26de248899d32437583ea7dc6d9e7b2ff1136fe2
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-24 23:26:31 +0800

    posted data to mailchimp API

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
+const secrets = require("./test.js");
 
 const app = express();
 app.use(express.static("public"));
@@ -11,12 +13,38 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-	var firstName = req.body.firstName;
-	var lastName = req.body.lastName;
-	var email = req.body.email;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
 
-	console.log(firstName + "\n" + lastName + "\n" + email);
-	res.send("post received");
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				}
+			}
+		]
+	};
+
+	const jsonData = JSON.stringify(data);
+	const url = "https://us21.api.mailchimp.com/3.0/lists/" + secrets.LISTID;
+	const options = {
+		method: "POST",
+		auth: "j-kenneth:" + secrets.APIKEY,
+	};
+
+	const request = https.request(url, options, function (response) {
+		response.on("data", function (data) {
+			console.log(JSON.parse(data));
+		});
+	});
+
+	request.write(jsonData);
+	request.end();
 });
 
 
